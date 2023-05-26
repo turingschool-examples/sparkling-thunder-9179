94a391351689e83132a67cd5fba489291e458a86
Myles Nottingham <MylesNottingham@gmail.com>
2023-05-26 12:30:58 -0600

    Add instance methods to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def actors_by_age
+    actors.order(:age)
+  end
+
+  def average_age_of_actors
+    actors.average(:age)
+  end
 end
