10a717a85adf7bca7f3c1f99d5fd66218413ac6e
Myles Nottingham <MylesNottingham@gmail.com>
2023-05-26 12:24:04 -0600

    Refactor studios index page

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
-RSpec.describe "the Studio index page" do
+RSpec.describe "the studios index page" do
   let!(:studio_1) { Studio.create!(name: "Universal Studios", location: "Hollywood") }
   let!(:studio_2) { Studio.create!(name: "Disney", location: "Orlando") }
 
-  let!(:movie_1) { studio_1.movies.create!(title: "Gremlins", creation_year: 1984, genre: "Horror") }
-  let!(:movie_2) { studio_1.movies.create!(title: "Jurassic Park", creation_year: 1993, genre: "Action/Adventure") }
+  let!(:movie_1) { studio_1.movies.create!(title: "Jurassic Park", creation_year: 1993, genre: "Action/Adventure") }
+  let!(:movie_2) { studio_1.movies.create!(title: "Gremlins", creation_year: 1984, genre: "Horror") }
   let!(:movie_3) { studio_2.movies.create!(title: "Toy Story", creation_year: 1995, genre: "Animation") }
   let!(:movie_4) { studio_2.movies.create!(title: "Avatar", creation_year: 2009, genre: "Science Fiction") }
 
@@ -19,15 +19,15 @@ RSpec.describe "the Studio index page" do
       expect(page).to have_content("Hollywood")
 
       within("#movie-#{movie_1.id}") do
-        expect(page).to have_content("Gremlins")
-        expect(page).to have_content("Year: 1984")
-        expect(page).to have_content("Genre: Horror")
-      end
-
-      within("#movie-#{movie_2.id}") do
         expect(page).to have_content("Jurassic Park")
         expect(page).to have_content("Year: 1993")
         expect(page).to have_content("Genre: Action/Adventure")
+      end
+
+      within("#movie-#{movie_2.id}") do
+        expect(page).to have_content("Gremlins")
+        expect(page).to have_content("Year: 1984")
+        expect(page).to have_content("Genre: Horror")
       end
     end
 
