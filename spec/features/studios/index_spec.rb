60d4aad7edb139120d94f1f548068215240bccf0
Nick Sacco <nicksacco10@gmail.com>
2023-05-26 12:48:33 -0700

    test: studio index tests written for us1

---
+++ spec/features/studios/index_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "Studio Index page /studios", type: :feature do
+  before(:each) do
+    test_data
+  end
+
+  describe "Studio Index page shows all studios and their attributes" do
+    it "can show each studio with each movie belonging to that studio" do
+      visit '/studios'
+
+      within "#studio_details_#{@studio_1.id}" do
+        expect(page).to have_content("Studio Name: #{@studio_1.name}")
+        expect(page).to have_content("Studio Location: #{@studio_1.location}")
+        expect(page).to have_content("Movies:")
+        expect(page).to have_content(@movie_1.title)
+        expect(page).to have_content(@movie_1.creation_year)
+        expect(page).to have_content(@movie_1.genre)
+      end
+
+      within "#studio_details_#{@studio_.id}" do
+        expect(page).to have_content("Studio Name: #{@studio_.name}")
+        expect(page).to have_content("Studio Location: #{@studio_.location}")
+        expect(page).to have_content("Movies:")
+        expect(page).to have_content(@movie_3.title)
+        expect(page).to have_content(@movie_3.creation_year)
+        expect(page).to have_content(@movie_3.genre)
+      end
+    end
+  end
+end
