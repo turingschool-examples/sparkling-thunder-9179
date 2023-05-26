9a8a8b66b08d895428628a23bfa4846a98255fc3
K.D. Hubbard <hubbard.kd@gmail.com>
2023-05-26 13:54:04 -0600

    add add actor form and route

---
+++ app/controllers/movie_actors_controller.rb
@@ -0,0 +1,8 @@
+class MovieActorsController < ApplicationController 
+  def create
+    movie = Movie.find(params[:id])
+    actor = Actor.find(params[:actor_id])
+    MovieActor.create!(movie: movie, actor: actor)
+    redirect_to "/movies/#{movie.id}"
+  end
+end
