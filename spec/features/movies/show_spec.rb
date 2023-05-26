afe0c9e81e73eeab1a87098498706ea27d272499
Myles Nottingham <MylesNottingham@gmail.com>
2023-05-26 12:58:52 -0600

    Add test for adding actor to movie

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "the movies show page" do
   let!(:actor_1) { movie_1.actors.create!(name: "Jeff Goldblum", age: 67) }
   let!(:actor_2) { movie_1.actors.create!(name: "Samuel L. Jackson", age: 71) }
   let!(:actor_3) { movie_1.actors.create!(name: "Laura Dern", age: 53) }
+  let!(:actor_4) { Actor.create!(name: "Richard Attenborough", age: 90) }
 
   before(:each) do
     visit "/movies/#{movie_1.id}"
@@ -26,6 +27,8 @@ RSpec.describe "the movies show page" do
     expect(page).to have_content("Age: #{actor_2.age}")
     expect(page).to have_content(actor_3.name)
     expect(page).to have_content("Age: #{actor_3.age}")
+    expect(page).not_to have_content(actor_4.name)
+    expect(page).not_to have_content("Age: #{actor_4.age}")
 
     expect(actor_3.name).to appear_before(actor_1.name)
     expect(actor_1.name).to appear_before(actor_2.name)
@@ -33,5 +36,21 @@ RSpec.describe "the movies show page" do
 
   it "displays the average age of all of the movie's actors" do
     expect(page).to have_content("Average Age of Actors: 64")
+  end
+
+  it "displays a form to add an actor to the movie by Actor ID" do
+    expect(page).to have_content("Add an Actor to this Movie")
+    expect(page).to have_field(:actor_id)
+    expect(page).to have_button("Submit")
+
+    fill_in :actor_id, with: actor_4.id
+    click_button "Submit"
+
+    expect(current_path).to eq("/movies/#{movie_1.id}")
+
+    expect(page).to have_content(actor_4.name)
+    expect(page).to have_content("Age: #{actor_4.age}")
+    expect(actor_2.name).to appear_before(actor_4.name)
+    expect(page).to have_content("Average Age of Actors: 70")
   end
 end
