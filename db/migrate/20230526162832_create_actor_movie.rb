360970a49082e8590019a44a5fd32d7baa90534c
Sarah Garlock <garlock.sarah10@gmail.com>
2023-05-26 10:40:38 -0600

    update schema and migrations

---
+++ db/migrate/20230526162832_create_actor_movie.rb
@@ -0,0 +1,10 @@
+class CreateActorMovie < ActiveRecord::Migration[7.0]
+  def change
+    create_table :actor_movies do |t|
+      t.references :actor, null: false, foreign_key: true
+      t.references :movie, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
