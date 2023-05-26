0e5dddee2935176dfa25ba357133129152e0543e
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-26 09:16:26 -0600

    Fix: test syntax

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe '/index' do
   before(:each) do
-    @pixar = Studio.create!{name: 'Pixar Animation Studios', location: 'California'}
+    @pixar = Studio.create!(name: 'Pixar Animation Studios', location: 'California')
 
-    @universal = Studio.create!{name: 'Universal Studios', location: 'Orlando'}
+    @universal = Studio.create!(name: 'Universal Studios', location: 'Orlando')
 
-    @the_dawn_of_netta = universal.movies.create!{title: 'The Dawn of Netta', creation_year: '1912', genre: 'silent drama'}
-    @when_bess_got_in_wrong = universal.movies.create!{title: 'When Bess Got in Wrong', creation_year: '1914', genre: 'silent comedy'}
+    @the_dawn_of_netta = universal.movies.create!(title: 'The Dawn of Netta', creation_year: '1912', genre: 'silent drama')
+    @when_bess_got_in_wrong = universal.movies.create!(title: 'When Bess Got in Wrong', creation_year: '1914', genre: 'silent comedy')
 
-    @ratatouille = pixar.movies.create!{title: 'Ratatouille', creation_year: '2007', genre: 'comedy'}
-    @inside_out = pixar.movies.create!{title: 'Inside Out', creation_year: '2015', genre: 'coming-of-age'}
+    @ratatouille = pixar.movies.create!(title: 'Ratatouille', creation_year: '2007', genre: 'comedy')
+    @inside_out = pixar.movies.create!(title: 'Inside Out', creation_year: '2015', genre: 'coming-of-age')
   end
 
   describe 'studio index' do
