0267a1e39f1269864483bfbef0c854dc9683c934
Amy Spears <amspears007@gmail.com>
2023-05-26 14:44:19 -0600

    Add actor info to view page

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -2,19 +2,25 @@ require "rails_helper"
 
 RSpec.describe "Movie Show Page" do
   let!(:studio1) {Studio.create!(name: "Univeral Studios", location: "Hollywood")}
-  let!(:studio2) {Studio.create!(name: "Warner Bros Studios", location: "Palm Springs")}
-  let!(:studio3) {Studio.create!(name: "MGM Studios", location: "Orlando")}
-
-
+  
   let!(:matrix) {studio1.movies.create!(title: "The Matrix", creation_year: 1999, genre: "Action")}
   let!(:speed) {studio1.movies.create!(title: "Speed", creation_year: 1994, genre: "Action")}
   let!(:always) {studio1.movies.create!(title: "Always Be My Maybe", creation_year: 2019, genre: "Rom Com")}
-  let!(:bill) {studio2.movies.create!(title: "Bill & Ted's Excellent Adventure", creation_year: 1990, genre: "comedy")}
-  let!(:point) {studio2.movies.create!(title: "Point Break", creation_year: 1991, genre: "Action")}
-  let!(:john) {studio3.movies.create!(title: "John Wick", creation_year: 2014, genre: "Thriller")}
+
+  let!(:keanu) {Actor.create!(name: "Keanu Reeves", age: 58)}
+  let!(:laurence) {Actor.create!(name: "Laurence Fishburne", age: 61)}
+  let!(:carrie) {Actor.create!(name: "Carrie-Anne Moss", age: 55)}
+  let!(:sandra) {Actor.create!(name: "Sandra Bullock", age: 58)}
+  let!(:ali) {Actor.create!(name: "Ali Wong", age: 41)}
+
+
+
+  # let!(:matrix_keanu) {MovieActor.create!(movie: "The Matrix", )}
 
   describe "US2 I see the movie's title, creation year, and genre," do
     it "I see a list of all its actors from youngest to oldest. And I see the average age of all of the movie's actors" do
+
+      matrix.actors << [keanu,laurence,carrie]
       visit "/movies/#{matrix.id}"
       save_and_open_page
 
@@ -32,6 +38,15 @@ RSpec.describe "Movie Show Page" do
         expect(page).to_not have_content("Rom Com")
         expect(page).to_not have_content("Point Break")
         #end
+
+      # within("#movie_actors-#{matrix.id}") do
+        expect(page).to have_content(keanu.name)
+        expect(page).to have_content(keanu.age)
+        expect(page).to have_content(laurence.name)
+        expect(page).to have_content(carrie.name)
+        expect(page).to_not have_content(ali.name)
+        expect(page).to_not have_content(sandra.name)
+      #end
       end
     end
   end
