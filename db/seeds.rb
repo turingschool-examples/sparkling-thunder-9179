48fc73f91585609f75d0d7b5c584bb21dbec55fa
Jesse Sorman <jessesorman@gmail.com>
2023-05-26 15:48:10 -0400

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-studio_1 = Studio.create!(name: "LucasArts", location: "LA")
+    studio_1 = Studio.create!(name: "LucasArts", location: "LA")
     movie_1 = studio_1.movies.create!(title: "StarWars", creation_year: "1977", genre: "Scifi")
     actor_1 = movie_1.actors.create!(name: "Mark Hamil", age: 26)
     actor_2 = movie_1.actors.create!(name: "Harrison Ford", age: 35)
     actor_3 = movie_1.actors.create!(name: "Carrie Fisher", age: 21)
+    studio_2 = Studio.create!(name: "some studio", location: "LA")
+    movie_2 = studio_2.movies.create!(title: "Action Movie", creation_year: "1980", genre: "Action")
+    actor_4 = movie_2.actors.create!(name: "Cool guy", age: 26)
+    actor_5 = movie_2.actors.create!(name: "Action dude", age: 29)
+    actor_6 = movie_2.actors.create!(name: "Tom Cruise", age: 30)
+
