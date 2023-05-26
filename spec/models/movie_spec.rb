5cff28ac6a604a54a107affedf5e8b54e9dd5751
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-26 11:36:31 -0600

    Test: #average_age

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Movie, type: :model do
     it {should belong_to :studio}
     it {should have_many :actors}
   end
+
+  describe "methods" do 
+    before(:each) do 
+      @universal = Studio.create!(name: 'Universal Studios', location: 'Orlando')
+      @when_bess_got_in_wrong = @universal.movies.create!(title: 'When Bess Got in Wrong', creation_year: '1914', genre: 'silent comedy')
+      @lee = @when_bess_got_in_wrong.actors.create!(name: 'Lee Moran', born: 1888, age: 22)
+      @bess = @when_bess_got_in_wrong.actors.create!(name: 'Bess Meredyth', born: 1890, age: 24)
+    end
+
+    it '#average_age' do 
+      expect(@when_bess_got_in_wrong.average_age).to eq(23)
+    end
+  end
 end
