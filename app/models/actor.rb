4ee276d13e2cc61787ff8dab357b74ba56548c41
Lauren Golden <llgolden@gmail.com>
2023-05-26 12:06:36 -0600

    Correct class names

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,4 @@
-class Movie < ApplicationRecord
+class Actor < ApplicationRecord
   has_many :movie_actors
   has_many :movies, through: :movie_actors
 end
--- spec/models/actors_spec.rb
+++ spec/models/actors_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Movie, type: :model do
+RSpec.describe Actor, type: :model do
   describe "relationships" do
     it { should have_many :movie_actors }
     it { should have_many(:movies).through(:movie_actors) }
