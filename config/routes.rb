0f855e42a7d96a70b44754bc527a71c46b69710f
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-05-26 15:03:18 -0600

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/studios", to: "studios#index"
+
+  get "/movies/:id", to: "movies#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
