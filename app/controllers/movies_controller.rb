b3a446ebeb4ba935dacda3d4309c4f8703646ac4
Steven Stackhouse <stackmm@gmail.com>
2023-05-26 14:49:21 -0600

    fixed create method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,12 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def create
+    movie = Movie.find(params[:id])
+    actor = Actor.find(params[:actor_id])
+
+    MovieActor.create!(movie: movie, actor: actor)
+    redirect_to "/movies/#{movie.id}"
+  end
 end
