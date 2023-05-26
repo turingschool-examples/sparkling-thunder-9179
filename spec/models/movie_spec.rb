0a3320588b4343ee34daa96f1fe9f0fd0ff061ea
Alex Kiselich <alexander.kiselich@gmail.com>
2023-05-26 12:04:53 -0600

    Added Actors and Movie_actors table and created relationships. Also created spec test for those models

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,5 +3,7 @@ require "rails_helper"
 RSpec.describe Movie, type: :model do
   describe "relationships" do
     it {should belong_to :studio}
+    it {should have_many :movie_actors}
+    it {should have_many(:actors).through(:movie_actors)}
   end
 end
