faf1b9a6127bce35a123fb55ce18f33f62ad530b
Steven Stackhouse <stackmm@gmail.com>
2023-05-26 13:28:30 -0600

    create test for movie instance method #actors_order_of_age

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Movie, type: :model do
     it {should have_many :movie_actors}
     it {should have_many(:actors).through(:movie_actors)}
   end
+
+  describe "instance methods" do
+    it "#actors_order_of_age" do
+      studio_1 = Studio.create!(name: "Disney", location: "Burbank, CA")
+      movie_1 = studio_1.movies.create!(title: "Star Wars", creation_year: "1972", genre: "Science Fiction")
+      actor_1 = movie_1.actors.create!(name: "Harrison Ford", age: 50)
+      actor_2 = movie_1.actors.create!(name: "Mark Hamill", age: 20)
+      actor_3 = movie_1.actors.create!(name: "Carrie Fisher", age: 30)
+
+      expect(movie_1.actors_order_of_age).to eq([actor_2, actor_3, actor_1])
+    end
+  end
 end
