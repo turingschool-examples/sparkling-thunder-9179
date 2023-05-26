3dd275efe089b1a4f7e9df80d5ad45682a1cffc8
Steven Stackhouse <stackmm@gmail.com>
2023-05-26 13:29:12 -0600

    create #actors_order_of_age instance method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def actors_order_of_age
+    actors.order("age")
+  end
 end
