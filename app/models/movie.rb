687634bda09e87968e5adb114e834ee6412946dc
Javen Wilson <javenb022@gmail.com>
2023-05-26 12:53:10 -0400

    creates movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,13 @@
 class Movie < ApplicationRecord
   belongs_to :studio
+  has_many :actor_movies
+  has_many :actors, through: :actor_movies
+
+  def average_age
+    actors.average(:age)
+  end
+
+  def actors_by_age
+    actors.order(:age)
+  end
 end
