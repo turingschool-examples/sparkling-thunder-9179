b9c5545f41162ed238b92385fbde2014c2354c68
Garrett Gregor <garrett.gregor@gmail.com>
2023-05-29 20:20:18 -0600

    US2: Test: movie show

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,12 +1,7 @@
 require "rails_helper"
 
-RSpec.describe Movie, type: :model do
-  describe "relationships" do
-    it {should belong_to :studio}
-    it {should have_many(:actors).through(:movie_actors) }
-  end
-
-  describe "instance methods" do
+RSpec.describe "/movies/:id", type: :feature do
+  describe "show" do
     before(:each) do
       @studio_1 = Studio.create!(name: "Universal Studios", location: "Hollywood")
       @movie_1 = @studio_1.movies.create!(title: "Jurassic Park", creation_year: "1995", genre: "action")
@@ -25,10 +20,26 @@ RSpec.describe Movie, type: :model do
       @movie_actor_3 = MovieActor.create!(movie_id: @movie_1.id, actor_id: @actor_3.id)
     end
 
-    context "#sort_by_age" do
-      it "sorts a movies actors by age" do
-        expect(@movie_1.actors_youngest_to_oldest).to eq([@actor_3, @actor_1, @actor_2])
-      end
+    # Story 2 - Movie Show
+
+    # As a user,
+    # When I visit a movie's show page.
+    # I see the movie's title, creation year, and genre,
+    # and a list of all its actors from youngest to oldest.
+    # And I see the average age of all of the movie's actors
+    it "display information for a particular movie (1)" do
+      visit "/movies/#{@movie_1.id}"
+
+      expect(page).to have_content("Movie Name: #{@movie_1.title}")
+      expect(page).to have_content("Creation Year: #{@movie_1.creation_year}")
+      expect(page).to have_content("Genre: #{@movie_1.genre}")
+    end
+
+    it "list all actors for a particular movie (1) from oldest to youngest" do
+      visit "/movies/#{@movie_1.id}"
+
+      expect(@actor_3.name).to appear_before(@actor_1.name)
+      expect(@actor_1.name).to appear_before(@actor_2.name)
     end
   end
 end
