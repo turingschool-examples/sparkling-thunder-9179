efb389df5e8520c4ecdb2adaf5537d6f61c9b9cf
Garrett Gregor <garrett.gregor@gmail.com>
2023-05-29 20:52:53 -0600

    US3: Feat: add actor to movie

---
+++ app/controllers/movie_actors_controller.rb
@@ -0,0 +1,6 @@
+class MovieActorsController < ApplicationController
+  def create
+    MovieActor.create!(actor_id: params[:actor_id], movie_id: params[:movie_id])
+    redirect_to "/movies/#{params[:movie_id]}"
+  end
+end
