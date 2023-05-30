caded21a839320e2a164b73cb0d2b696ae1b78a9
Garrett Gregor <garrett.gregor@gmail.com>
2023-05-29 20:20:29 -0600

    US2: Feat: movie show

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def actors_youngest_to_oldest
+    actors.order(:age)
+  end
 end
